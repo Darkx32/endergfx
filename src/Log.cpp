e4bd9730056a21dcff67a701b0b0f16b66cbb50a
thecogumeta <the.cogumeta+github@gmail.com>
2026-09-06 02:13:56 -0300

    add configurable log levels

--- src/Log.cpp
+++ src/Log.cpp
@@ -1,5 +1,7 @@
 #include "endergfx/Log.hpp"
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 namespace endergfx {
 
@@ -29,13 +31,41 @@ void defaultLogCallback(LogLevel level, const std::string &message) {
                message.c_str());
 }
 
+LogLevel detectDefaultLogLevel() {
+  if (std::getenv("ENDERGFX_DEBUG")) {
+    return LogLevel::Debug;
+  }
+  const char *env = std::getenv("ENDERGFX_LOG_LEVEL");
+  if (!env) {
+    return LogLevel::Warning;
+  }
+  if (std::strcmp(env, "debug") == 0)
+    return LogLevel::Debug;
+  if (std::strcmp(env, "info") == 0)
+    return LogLevel::Info;
+  if (std::strcmp(env, "warning") == 0)
+    return LogLevel::Warning;
+  if (std::strcmp(env, "error") == 0)
+    return LogLevel::Error;
+  return LogLevel::Warning;
+}
+
+LogLevel g_minLevel = detectDefaultLogLevel();
+
 } // namespace
 
 void setLogCallback(LogCallback callback) {
   g_logCallback = std::move(callback);
 }
 
+void setMinLogLevel(LogLevel level) { g_minLevel = level; }
+
+LogLevel getMinLogLevel() { return g_minLevel; }
+
 void log(LogLevel level, const std::string &message) {
+  if (level < g_minLevel) {
+    return;
+  }
   if (g_logCallback) {
     g_logCallback(level, message);
   } else {
